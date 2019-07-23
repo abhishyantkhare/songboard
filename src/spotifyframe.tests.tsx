69054e084b576a6770ea2766e121bd48579e1ff8
Abhishyant Khare <Abhishyant Khare>
2019-07-23 13:33:46 -0700

    add spotify test

--- src/spotifyframe.tests.tsx
+++ src/spotifyframe.tests.tsx
@@ -1,3 +1,11 @@
 import React from 'react'
 import SpotifyFrame from './spotifyframe'
 import {shallow} from 'enzyme'
+
+
+describe('<SpotifyFrame />', () => {
+  it('returns an iframe'), () => {
+    const wrapper = shallow(<SpotifyFrame url=""/>);
+    expect(wrapper.find(<iframe />))
+  }
+})
